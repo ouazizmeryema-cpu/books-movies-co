cf727b20b9237710868f83f9af1c2bd55be603a4
meriama <ouazizmeryema@gmail.com>
2026-04-08 10:49:18 +0200

    Config :  connexion MySQL db.js

---
+++ backend/config/db.js
@@ -0,0 +1,23 @@
+var mysql = require('mysql2');
+var con = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME,
+    port: process.Env.DB_PORT
+
+});
+
+con.connect(err => {
+    if(err) {
+         console.log('Erreur connexion Mysql', err);
+    }else{
+        console.log('Connecté à MySQL');
+    };
+   
+    
+
+});
+
+module.exports =db;
+
