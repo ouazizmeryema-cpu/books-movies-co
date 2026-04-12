208ced7822f7f97c1ffaa3d3a1be95802cec5202
EmmaC <emma.callig@gmail.com>
2026-04-12 19:25:18 +0200

    StarRating fonctionnel mais améliorable, besoin de la bdd

--- frontend/src/components/StarRating.jsx
+++ frontend/src/components/StarRating.jsx
@@ -1,10 +1,13 @@
-import { useState } from "react";
-import { createNote } from "../services/notesService";
+import { useState, useEffect } from "react";
+import { getNoteById, createNote } from "../services/notesService";
 
 function StarRating({ film }) {
     const user = JSON.parse(localStorage.getItem("user") || "null");
-    const user_id = user.id;
+
+    const user_id = user?.id;
     const film_id = film.id;
+
+    const [note_film, setNoteFilm] = useState(null);
 
     const [scenario, setScenario] = useState(1);
     const [jeu_acteur, setJeuActeur] = useState(1);
@@ -13,20 +16,37 @@ function StarRating({ film }) {
     const notation = async (e) => {
         e.preventDefault();
         
-        const newNote = {user_id, film_id, scenario, jeu_acteur, quali_audio};
+        const newNote = {
+            utilisateurs_id: user_id,
+            films_id: film_id,
+            scenario,
+            jeu_acteur,
+            qualite_audiovisuelle: quali_audio
+        };
         await createNote(newNote); 
-                    
-        location.reload();
+        setNoteFilm(res.data);
     }
+
+    const formulaire = (
+        <form onSubmit={notation}>
+            <label>Scénario :<input type="number" value={scenario} min="1" max="5" step="1" onChange={(e) => setScenario(Number(e.target.value))} required />/5</label>
+            <label>Jeu d'acteur :<input type="number" value={jeu_acteur} min="1" max="5" step="1" onChange={(e) => setJeuActeur(Number(e.target.value))} required />/5</label>
+            <label>Qualité audiovisuel :<input type="number" value={quali_audio} min="1" max="5" step="1" onChange={(e) => setQualiAudio(Number(e.target.value))} required />/5</label>
+            <button type="submit">Enregistrer ma note</button>
+        </form>
+    )
+
+
+    useEffect(() => {
+        getNoteById(user.id, film_id).then((res) => {
+        setNoteFilm(res.data);
+        });
+    }, [film_id, user]);
 
     return (
         <div>
-            <form onSubmit={notation}>
-                <label>Scénario :<input type="number" value={scenario} min="1" max="5" step="1" onChange={(e) => setScenario(e.target.value)} required />/5</label>
-                <label>Jeu d'acteur :<input type="number" value={jeu_acteur} min="1" max="5" step="1" onChange={(e) => setJeuActeur(e.target.value)} required />/5</label>
-                <label>Qualité audiovisuel :<input type="number" value={quali_audio} min="1" max="5" step="1" onChange={(e) => setQualiAudio(e.target.value)} required />/5</label>
-                <button type="submit">Enregistrer ma note</button>
-            </form>
+            {!note_film && formulaire}
+            {note_film && <p>Vous avez déjà noté ce film</p>}
         </div>
     );
 }
--- frontend/src/pages/FilmDetailPage.jsx
+++ frontend/src/pages/FilmDetailPage.jsx
@@ -1,28 +1,18 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { getFilmById } from "../services/filmsService";
-import { getNoteById } from "../services/notesService";
 import StarRating from "../components/StarRating";
 
 function FilmDetailPage() {
   const { id } = useParams();
 
-  const user = JSON.parse(localStorage.getItem("user") || "null");
-
   const [film, setFilm] = useState(null);
-  const [note_film, setNoteFilm] = useState(null);
 
   useEffect(() => {
     getFilmById(id).then((res) => {
       setFilm(res.data);
     });
   }, [id]);
-
-  useEffect(() => {
-    getNoteById(user.id, id).then((res) => {
-      setNoteFilm(res.data);
-    });
-  }, [id, user]);
 
   if (!film) {
     return <p>Chargement...</p>;
@@ -38,8 +28,7 @@ function FilmDetailPage() {
       <p>Commentaire : {film.commentaire}</p>
       <p>Nombre de votes : {film.nb_votes}</p>
       <p>Année de sortie : {film.annee_sortie}</p>
-      {!note_film && <StarRating film={film} />}
-      {note_film && <p>Vous avez déjà noté ce film</p>}
+      {<StarRating film={film} />}
     </div>
   );
 }
