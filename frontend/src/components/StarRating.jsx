ec59e1625c9c961be59985dcec8df293a30187dd
EmmaC <emma.callig@gmail.com>
2026-04-13 11:31:06 +0200

    amélioration StarRating

--- frontend/src/components/StarRating.jsx
+++ frontend/src/components/StarRating.jsx
@@ -8,6 +8,7 @@ function StarRating({ film }) {
     const film_id = film.id;
 
     const [note_film, setNoteFilm] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     const [scenario, setScenario] = useState(1);
     const [jeu_acteur, setJeuActeur] = useState(1);
@@ -38,15 +39,26 @@ function StarRating({ film }) {
 
 
     useEffect(() => {
-        getNoteById(user.id, film_id).then((res) => {
-        setNoteFilm(res.data);
-        });
+        setLoading(true);
+
+        getNoteById(user.id, film_id)
+            .then((res) => {
+                setNoteFilm(res.data);
+            })
+            .catch(() => {
+                setNoteFilm(null);
+            })
+            .finally(() => {
+                setLoading(false);
+            });
     }, [film_id, user]);
+
 
     return (
         <div>
-            {!note_film && formulaire}
-            {note_film && <p>Vous avez déjà noté ce film</p>}
+            {loading && <p>Chargement...</p>}
+            {!loading && note_film && (<p>Vous avez déjà noté ce film</p>)}
+            {!loading && !note_film && formulaire}
         </div>
     );
 }
