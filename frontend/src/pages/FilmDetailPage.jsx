faad2aae4ddf3af1e9096b6bdaea3d9e68b5a083
EmmaC <emma.callig@gmail.com>
2026-04-13 14:38:26 +0200

    Revert "css de FilmDetailPage"
    This reverts commit 31e1ba74c5ce05b16812682690c7948109e1b5ee.

--- frontend/src/pages/FilmDetailPage.jsx
+++ frontend/src/pages/FilmDetailPage.jsx
@@ -2,8 +2,6 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { getFilmById } from "../services/filmsService";
 import StarRating from "../components/StarRating";
-import '../styles/FilmDetailPage.css'
-import '../styles/all.css'
 
 function FilmDetailPage() {
   const { id } = useParams();
